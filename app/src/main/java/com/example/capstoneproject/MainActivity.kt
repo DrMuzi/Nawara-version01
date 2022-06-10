02f3d4e889e3778dfc4c2a910763dc96c0a30d90
DR_Muzi <dimasrafimuzi@gmail.com>
2022-06-10 18:00:47 +0700

    first commit

---
+++ app/src/main/java/com/example/capstoneproject/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.capstoneproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.capstoneproject.auth.AuthenticationActivity
+import com.example.capstoneproject.databinding.ActivityMainBinding
+import com.example.capstoneproject.util.UserUtil
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*
+        * If the user is not logged In, then taking to AuthenticationActivity
+        * */
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            startActivity(Intent(this, AuthenticationActivity::class.java))
+        }
+
+        UserUtil.getCurrentUser()
+
+        /*
+        * Default Fragment
+        * */
+        setFragment(FeedFragment())
+
+        /*
+        * Bottom Navigation View
+        * */
+        binding.navigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.feed_item -> {
+                    setFragment(FeedFragment())
+                }
+                R.id.search_item -> {
+                    setFragment(SearchFragment())
+                }
+                R.id.chatroom_item -> {
+                    setFragment(ChatroomFragment())
+                }
+                R.id.profile_item -> {
+                    setFragment(ProfileFragment())
+                }
+            }
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+    private fun setFragmentWithBackStack(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+}
