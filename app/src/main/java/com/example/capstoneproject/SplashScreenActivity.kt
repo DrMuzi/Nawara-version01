1d93be467ff8526462e5f3f3e4460f34925eb830
DrMuzi <dimasrafimuzi@gmail.com>
2022-06-13 07:41:25 +0700

    Add Splash Screen and On Boarding

---
+++ app/src/main/java/com/example/capstoneproject/SplashScreenActivity.kt
@@ -0,0 +1,51 @@
+package com.example.capstoneproject
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var topAnimation: Animation
+    private lateinit var buttomAnimation: Animation
+    private lateinit var imageLogo: ImageView
+    private lateinit var slogan : TextView
+
+    @Suppress("DEPRECATION")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        supportActionBar?.hide()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        buttomAnimation = AnimationUtils.loadAnimation(this, R.anim.buttom_animation)
+
+        imageLogo = findViewById(R.id.logo)
+        slogan = findViewById(R.id.slogan_text)
+
+        imageLogo.startAnimation(topAnimation)
+        slogan.startAnimation(buttomAnimation)
+        Handler().postDelayed({
+            if (FirebaseAuth.getInstance().currentUser != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+            else{
+            val intent = Intent(this, OnBoardingActivity::class.java)
+            startActivity(intent)
+            finish()}
+        }, 2000)
+    }
+}
